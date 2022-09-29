da29cc8205bee395bd3ee83fd2fef7fd20c2b8c0
luciosantiagovanderlei <luciosantiagovandelei@gmail.com>
2022-09-28 21:32:55 -0300

    Consulta com vários critérios

--- src/main/java/com/educandoweb/workshopmongo/resouces/PostResource.java
+++ src/main/java/com/educandoweb/workshopmongo/resouces/PostResource.java
@@ -1,5 +1,6 @@
 package com.educandoweb.workshopmongo.resouces;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,7 +33,25 @@ public class PostResource {
 	}
 	
 	@RequestMapping(value="/titlesearch", method = RequestMethod.GET)
-	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue = "") String text){	
+	public ResponseEntity<List<Post>> findByTitle(
+			@RequestParam(value="text", defaultValue = "") String text,
+			@RequestParam(value="minDate", defaultValue = "") String minDate,
+			@RequestParam(value="maxDate", defaultValue = "") String maxDate)	
+	
+	{	
+		
+		text = URL.decodeparam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		
+		List<Post> list = service.fullSearch(text, min, max);
+		
+		return ResponseEntity.ok().body(list);
+				
+	}
+	
+	@RequestMapping(value="/fullsearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullsearch(@RequestParam(value="text", defaultValue = "") String text){	
 		
 		text = URL.decodeparam(text);
 		List<Post> list = service.findByTitle(text);
@@ -40,6 +59,7 @@ public class PostResource {
 		return ResponseEntity.ok().body(list);
 				
 	}
+	
 	
 	
 	
